246b479674623431abf25b5f657249b8ad356ee6
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-16 02:25:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,6 +110,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:dennis_ritchie][:known_for] = alans_new_info
+programmer_hash
 end
 
 def adding_to_dennis
@@ -128,5 +129,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash
 end
