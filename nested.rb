3e42312e30fd9edebf754d32e4b541da9a0e9426
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-16 02:27:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,8 +109,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmer_hash[:dennis_ritchie][:known_for] = alans_new_info
-programmer_hash
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -129,5 +129,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash
+programmer_hash[:dennis_ritchie][:known_for]
 end
